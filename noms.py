3ac3f24f4dc3495ea19d53d39039e3e814c12853
Harun796 <harun.meraga@iesjoandaustria.org>
2020-04-16 08:39:27 +0000

    he fet l'exercici 4

--- noms.py
+++ noms.py
@@ -1,12 +1,31 @@
 #Definiu la variable que ens guardarà l'estadística per noms.
+noms = {}
+vegades = 0
 #Pista clau ha de ser el nom i valor els cops que apareix. 
 
 #Heu de llegir el fitxer noms.
-
 #Aneu llegint  línia a lína que correspon a un nom.
+fitxer = open("noms.txt","r")
 #Sumeu cap cop que apareix un nom al seu valor.
+for linia in fitxer:
+  if(linia not in noms):
+    noms[linia] =  1 
+  else:
+    vegades = noms[linia]
+    noms[linia] = vegades + 1
+
 
 #Finalment pinteu tots els noms i amb els cops que s'ha posat el darrer any.
+print(noms)
 #Pintar només la variable. Els diccionaris ja surten ordenats alfabèticament.
 
+#noms { "javi":3,"maria":4,"pepe":5}
+
+#noms = {}
+
+#linia -> "javi"
+
+#si extsteix diccionari
+#vegades = noms[linia]
+#moms[linia] = vegades + 1
 
